bd872442ed75032831246a38ce2790d6961efba6
Mario Montiel <mario159_@hotmail.com>
2022-03-31 12:22:12 -0600

    fixed the desktop demo 1 website

--- api/secret.tsx
+++
@@ -1,3 +0,0 @@
-export const secret = '2a9e7047-804c-48ca-852a-d0d9cb035ad7'
-
-export const userSecret = '400dfb8f-cbe0-42d8-8431-38933d8dab92'
--- config/googleMaps/apiKey.ts
+++
@@ -1,7 +0,0 @@
-import { Loader } from "@googlemaps/js-api-loader"
-
-export const apiKey = new Loader ({
-    apiKey: "AIzaSyCP_S-hIPc7Ppk0k47TYzbkCkBw8TfnD-c",
-    version: "weekly",
-    libraries: ["places"]
-})
