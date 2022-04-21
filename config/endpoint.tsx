5cf745527db2d52ec3b53417975c31fdf6a21cb1
Mario Montiel <mario159_@hotmail.com>
2022-04-20 21:30:15 -0500

    change endpoint

--- config/endpoint.tsx
+++ config/endpoint.tsx
@@ -1,3 +1,7 @@
-export const endpoint: string = 'http://localhost:3000'
+export const endpoint: string = 'https://hoteles-plaza-demo.herokuapp.com'
+
+// http://localhost:3000
 
 // 3.131.206.162
+
+// hoteles-plaza-demo.herokuapp.com
